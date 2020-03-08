8481cb88ef3269778a31c770a2bc9377abb3d17a
Dmitry Lebetsky <dmitriy.lebetski@mail.ru>
2020-03-08 13:45:08 +0300

    docs: finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let codedLetters = {};
+    let exprArr = expr.split(""); 
+    let numberOfLetters = expr.length / 10;
+    for(let i = 0; i < numberOfLetters; i++) {
+        let from = i*10;
+        let until = from + 10;
+        codedLetters[i+1] = exprArr.slice(from, until).join("");
+    }
+    for(let i in codedLetters) {
+        if (codedLetters[i].indexOf(1) !== -1) {
+            let from = codedLetters[i].indexOf(1);
+            codedLetters[i] = codedLetters[i].slice(from);
+        }
+    }
+    for(let i in codedLetters) {
+        while(codedLetters[i].includes('10')) {
+            codedLetters[i] = codedLetters[i].replace('10', '.');
+        }
+    }
+    for(let i in codedLetters) {
+        while(codedLetters[i].includes('11')) {
+            codedLetters[i] = codedLetters[i].replace('11', '-');
+        }
+    }
+    for(let i = 1; i <= numberOfLetters; i++) {
+        if(codedLetters[i] == '**********') {
+            result = result + ' ';
+        } else {
+            result = result + MORSE_TABLE[codedLetters[i]];
+        }
+    }
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
